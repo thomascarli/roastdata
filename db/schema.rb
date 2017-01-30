9bed9d051f5ec1aa2731e62d4b91609841007cb9
Sebastian Valdez <valdez.sebastian4@gmail.com>
2017-01-30 00:23:18 -0800

    implemet Devise gem and generate Devise Users model

---
+++ db/schema.rb
@@ -0,0 +1,65 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20170130082114) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "brews", force: :cascade do |t|
+    t.integer  "roast_id"
+    t.string   "brew_method"
+    t.datetime "time_of_brew"
+    t.integer  "qty_of_beans"
+    t.integer  "bitterness"
+    t.integer  "complexity"
+    t.integer  "acidity"
+    t.integer  "overall"
+    t.string   "texture"
+    t.text     "comments"
+    t.integer  "user_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  create_table "roasts", force: :cascade do |t|
+    t.string   "roast_method"
+    t.datetime "date_of_roast"
+    t.string   "bean_species"
+    t.integer  "roast_time"
+    t.datetime "time_of_roast"
+    t.string   "roast_profile"
+    t.integer  "avg_temp_roast"
+    t.integer  "raw_weight"
+    t.string   "storage"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  end
+
+end
