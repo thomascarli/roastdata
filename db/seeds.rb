0ba668230e4ed984c2dc7a2ab7039024ce36c723
Sebastian Valdez <valdez.sebastian4@gmail.com>
2017-02-02 09:58:21 -0800

    created seeds.rb,run:'bundle exec rake db:reset'

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,20 @@
-# @user = User.create(email: "seb@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "seb", last_name: "vez")
+@user1 = User.create(email: "seb@test.com", password: "asdfasdf", password_confirmation: "asdfasdf") 
+@user2 = User.create(email: "tom@test.com", password: "asdfasdf", password_confirmation: "asdfasdf")
+puts "2 Users created" + "\n\n"
+puts "look at config/db/seeds.rb for user names & passwords " + "\n\n"
 
-# puts "1 User created"
+ 50.times do |roast|
+ 	Roast.create!(
+		roast_method: "roastyToasty-#{roast}",
+		date_of_roast: Date.today,
+		bean_species: 'Arabica',
+		roast_time: 720,
+		time_of_roast: Date.today,
+		roast_profile: 'Medium',
+		avg_temp_roast: 450,
+		raw_weight: 90,
+		storage: 'Jar',
+		user_id: @user2.id)
+ end
 
-# 100.times do |post|
-# 	Roast.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
-# end
-
-# puts "100 Posts have been created"
+ puts "50 Roasts have been created!"
