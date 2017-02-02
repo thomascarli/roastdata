8adb879afd3f15eaea4095c73f0739506fdd3f3e
Thomas Carli <thomas@procore.com>
2017-02-02 16:34:01 +0000

    add brews

--- app/controllers/brew_controller.rb
+++ app/controllers/brew_controller.rb
@@ -1,9 +1,21 @@
 class BrewController < ApplicationController
 
   def index
+    @brews = roast.brews
+  end
+
+  def show
+    roast
+    @brew = Brew.find(params[:id])
   end
 
   def create
+  end
+
+  private
+
+  def roast
+    @roast ||= Roast.find(params[:roast_id])
   end
 
 end
