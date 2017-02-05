91bd815d2e597eeb3eba81f7e4ee660ffe139f04
Thomas Carli <thomas@procore.com>
2017-02-05 08:26:27 +0000

    assign default user type

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,7 +26,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-         
+
   has_many :brews
   belongs_to :user_type
+
+  before_validation :assign_user_type
+
+  def assign_user_type
+    self.user_type = UserType.find_by(name: "Standard")
+  end
 end
